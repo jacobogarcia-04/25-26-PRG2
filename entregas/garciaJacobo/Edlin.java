454416d5800b88929bc354ebd886ac4e33465696
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-02-13 10:46:19 +0100

    refactor: metodo estatico para preguntarLineaActiva y metodo estatico ediatr

--- entregas/garciaJacobo/Edlin.java
+++ entregas/garciaJacobo/Edlin.java
@@ -1,33 +1,58 @@
 package entregas.garciaJacobo;
 
+import java.util.Scanner;
+
 public class Edlin {
     public static void main(String[] args) {
-        String[] fichero = new String[10];
+        String[] fichero = {
+                "Bienvenidos al editor EDLIN",
+                "Utilice el menu inferior para editar el texto",
+                "------",
+                "[L] permite definir la linea activa",
+                "[E] permite editar la linea activa",
+                "[I] permite intercambiar dos lineas",
+                "[B] borra el contenido de la linea activa",
+                "[D] deshace la última acción realizada",
+                "[S] sale del programa",
+                ""
+        };
 
-        fichero[0] = "Bienvenidos al editor EDLIN";
-        fichero[1] = "Utilice el menu inferior para editar el texto";
-        fichero[2] = "------";
-        fichero[3] = "[L] permite definir la linea activa";
-        fichero[4] = "[E] permite editar la linea activa";
-        fichero[5] = "[I] permite intercambiar dos lineas";
-        fichero[6] = "[B] borra el contenido de la linea activa";
-        fichero[7] = "[D] deshace la última acción realizada";
-        fichero[8] = "[S] sale del programa";
-        int[] lineaActiva = { 1 };
+        int[] lineaActiva = new int[1];
+
+        mostrarMenu(fichero, lineaActiva);
+        preguntarLineaActiva(lineaActiva);
         mostrarMenu(fichero, lineaActiva);
 
+    }
+
+    private static void preguntarLineaActiva(int[] lineaActiva) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Que linea activa eliges?");
+        lineaActiva[0] = scanner.nextInt();
+
+    }
+
+    private static void editar(int[] lineaActiva, String[] fichero) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Que linea activa eliges?");
+        fichero[lineaActiva[0]] = scanner.nextLine();
     }
 
     private static void mostrarMenu(String[] fichero, int[] lineaActiva) {
         imprimirBordeHorizontal();
         for (int i = 0; i < fichero.length; i++) {
-            
-            String separador= i + ((lineaActiva[0]==i) ? "*": "")+ "|";
-            System.out.println(separador+fichero[i]);
+
+            String separador = i + ((lineaActiva[0] == i) ? "*" : " ") + "|";
+            if (fichero[i] != null) {
+
+                System.out.println(separador + fichero[i]);
+            } else {
+                System.out.println(separador);
+            }
+
         }
         imprimirBordeHorizontal();
         imprimirComandos();
-
     }
 
     private static void imprimirComandos() {
