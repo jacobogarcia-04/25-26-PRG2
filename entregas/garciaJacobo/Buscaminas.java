98c5b2b814c3562e83019b9d094ab666d219b7d6
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-02-17 13:29:08 +0100

    Se crea metodo para colocar las minas de forma aleatoria con un numero maximo de 3 minas

--- entregas/garciaJacobo/Buscaminas.java
+++ entregas/garciaJacobo/Buscaminas.java
@@ -12,7 +12,8 @@ public class Buscaminas {
                 { -1, -1, -1, -1, -1, -1, -1 }
 
         };
-
+        final int NUMERO_MINAS=3;
+        
         mostrarMapa(superficie);
     }
 
@@ -35,4 +36,19 @@ public class Buscaminas {
         }
         System.out.println("  ===================");
     }
+
+
+
+    private static void colocarMinas(int[][] superficie, int NUMERO_MINAS) {
+        int minasColocadas = 0;
+        do {
+            int x = (int) (Math.random() * superficie[0].length);
+            int y = (int) (Math.random() * superficie.length);
+            if (superficie[y][x] != -2) {
+                superficie[y][x] = -2;
+                minasColocadas++;
+            }
+
+        } while (minasColocadas < NUMERO_MINAS);
+}
 }
