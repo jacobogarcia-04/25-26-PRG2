32d090812218b35399e7eb242b11a3e754d1feb1
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-02-17 13:14:40 +0100

    Se crean 2 métodos estáticos:
    - mostrarMapa: recorre la matriz bidimensional y la muestra por pantalla.
    - mapearSuperficie: convierte los valores de la matriz en símbolos:
        - -1 → "-" (casilla oculta)
        - 0 o casilla descubierta sin bomba → "."
        - mina → "*"

--- entregas/garciaJacobo/Buscaminas.java
+++ entregas/garciaJacobo/Buscaminas.java
@@ -11,5 +11,22 @@ public class Buscaminas {
                 { -1, -1, -1, -1, -1, -1, -1 }
 
         };
+     
+        mostrarMapa(superficie);
+    }
+
+    private static String mapearSuperficie(int nºmatriz) {
+        final String TILES[] = { " - ", ".", "*" };
+        return nºmatriz < 0 ? TILES[0] : TILES[nºmatriz];
+    }
+
+    private static void mostrarMapa(int[][] superficie) {
+        for (int i = 0; i < superficie.length; i++) {
+            for (int j = 0; j < superficie[i].length; j++) {
+                int tile = superficie[i][j];
+                System.out.print(mapearSuperficie(tile));
+            }
+            System.out.println();
+        }
     }
 }
