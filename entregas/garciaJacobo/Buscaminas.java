2d48371a30578975f9656aefd28db7fc0324cfae
jacobogarcia-04 <jacobo.garcia@alumnos.uneatlantico.es>
2026-02-17 12:54:43 +0100

    se crea matriz bidimensional para el tablero del juego

---
+++ entregas/garciaJacobo/Buscaminas.java
@@ -0,0 +1,15 @@
+package entregas.garciaJacobo;
+
+public class Buscaminas {
+    public static void main(String[] args) {
+
+        int[][] superficie = {
+                { -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1 },
+                { -1, -1, -1, -1, -1, -1, -1 }
+
+        };
+    }
+}
